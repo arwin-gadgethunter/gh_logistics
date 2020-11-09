68f0b5315277cf04fc35dab8ce72eaef6f36a447
arwin-gadgethunter <arwin.gadgethunter@gmail.com>
2020-11-09 18:52:30 +0800

    registration

--- resources/coreui/src/views/order/create_order/CreateOrder.js
+++ resources/coreui/src/views/order/create_order/CreateOrder.js
@@ -1,111 +1,120 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
-  CButton,
-  CCard,
-  CCardBody,
-  CCardFooter,
-  CCardHeader,
-  CCol,
-  CForm,
-  CFormGroup,
-  CFormText,
-  CTextarea,
-  CInput,
-  CInputFile,
-  CInputCheckbox,
-  CInputRadio,
-  CLabel,
-  CSelect,
-  CRow,
-  CSwitch,
-  CDataTable
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-//import { DocsLink } from 'src/reusable'
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from 'reactstrap'
 
-const BasicForms = () => {
-  const [collapsed, setCollapsed] = React.useState(true)
-  const [showElements, setShowElements] = React.useState(true)
+import SuccessAlert from '../../alert/SuccessAlert'
 
-  return (
-    <>
-      <CRow>
-        <CCol xs="12" md="12">
-          <CCard>
-            <CCardHeader>
+const initialState = {
+  name: "",
+  nameError: "",
+};
+
+export default class ValiationForm extends Component {
+  state = initialState;
+
+  constructor()
+  {
+    super();
+    this.state = {
+      rider: [],
+      newRiderData:{
+        name:"",
+      },
+    }
+  }
+
+  handleChange = event => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value
+    });
+  };
+
+
+  validate = () => {
+    let nameError = "";
+
+    if (!this.state.name) {
+      nameError = "Customer cannot be blank";
+    }
+
+    if (nameError) {
+      this.setState({ nameError });
+      return false;
+    }
+
+    return true;
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+
+    axios.post('http://127.0.0.1:8000/api/create_order/', {
+      name: this.state.name,
+    })
+    this.setState({
+      alert_message:"success",
+    })
+    this.setState(initialState);
+    }
+  };
+  
+
+  render() {
+    return (
+      <div>
+      <Row>
+        <Col xs="12" md="12">
+          <Card>
+            <CardHeader>
               General Info
-            </CCardHeader>
-            <CCardBody>
-              <CForm action="" method="post" encType="multipart/form-data" className="form-horizontal">
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="text-input">Customer Name</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CInput id="text-input" name="text-input" placeholder="Text" />
-                    <CFormText>This is a help text</CFormText>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="email-input">Date of Order</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CInput type="date" id="date-input" name="date-input" placeholder="Enter Email" autoComplete="email" />
-                    <CFormText className="help-block">Please enter date of order</CFormText>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="select">Currency</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CSelect custom name="select" id="select">
-                      <option value="1">PHP</option>
-                    </CSelect>
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="date-input">Date Input</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CInput type="date" id="date-input" name="date-input" placeholder="date" />
-                  </CCol>
-                </CFormGroup>
-                <CFormGroup row>
-                  <CCol md="3">
-                    <CLabel htmlFor="text-input">Rider Name</CLabel>
-                  </CCol>
-                  <CCol xs="12" md="9">
-                    <CInput id="text-input" name="text-input" placeholder="Text" />
-                    <CFormText>This is a help text</CFormText>
-                  </CCol>
-                </CFormGroup>
-              </CForm>
-            </CCardBody>
- 
+            </CardHeader>
+            <CardBody>
+            {this.state.alert_message=="success"?<SuccessAlert/>:null}
+              <Form action="" onSubmit={this.handleSubmit} method="post" encType="multipart/form-data" className="form-horizontal">
+                <FormGroup row>
+                  <Col md="3">
+                    <Label htmlFor="text-input">Customer Name</Label>
+                  </Col>
+                  <Col xs="12" md="9">
+                    <Input id="text-input" name="name"
+                      placeholder="name"
+                      value={this.state.name}
+                      onChange={this.handleChange} placeholder="Text" />
+                    <p className="text-warning">{this.state.nameError}</p>
+                  </Col>
+                </FormGroup>
 
-          <CCardHeader>
+                <Button type="submit" size="sm" color="primary" className="float-right"> Add Item</Button>
+              </Form>
+            </CardBody>
+
+          <CardHeader>
               Item List
-              <CButton type="submit" size="sm" color="primary" className="float-right"> Add Item</CButton>
-            </CCardHeader>
-          <CCardBody>
-            <CDataTable  
-              />
-
-              <CCol col="2" className="mb-3 mb-xl-0 text-center">
-                <CButton color="primary" size="lg">Save Order</CButton>
-              </CCol>
-            </CCardBody>
-
-            
-        </CCard>
-        </CCol>
-        
-    </CRow>
-    </>
-  )
+            </CardHeader>
+          <CardBody>
+              <Col col="2" className="mb-3 mb-xl-0 text-center">
+                <Button color="primary" size="lg">Save Order</Button>
+              </Col>
+            </CardBody>
+        </Card>
+        </Col>
+    </Row>
+    </div>
+    );
+  }
 }
-
-export default BasicForms
